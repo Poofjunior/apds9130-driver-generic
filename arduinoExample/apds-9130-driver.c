9277340c8fb40109f774db614561855ef95f4349
poofjunior <seeifyoucanmeetme@gmail.com>
2014-10-26 22:47:31 -0700

    arduino example is compiling as C code, but files structure is wrong.

---
+++ arduinoExample/apds-9130-driver.c
@@ -0,0 +1,78 @@
+/**
+ * \project apds9130-driver
+ * \brief   a generic driver providing the most basic functionality 
+ *          for the vl6180 proximity sensor. In a nutshell, you 
+ *          implement the three functions read(...) and write(...), 
+ *          and delayMS(...) on the microcontroller of your choice, 
+ *          and the rest is done for you.
+ * \author Joshua Vasquez
+ * \date October 25, 2014
+*/
+
+#include "apds-9130-driver.h"
+struct sensorParams apds9130_Params =
+{                                                                               
+    0x39,   // device address (cannot be changed)                               
+                                                                                
+    0xff,   // proxIntegrationTime                                              
+    0xff,   // waitTime                                                         
+    0x01,   // proxPulseCount                                                   
+                                                                                
+    0x0,    // proxDriveCurrent                                                 
+    0x20,   // proxDiodeSelect (cannot be changed)                              
+    0x0,    // proxGain                                                         
+                                                                                
+    0x0,    // waitEnable                                                       
+    0x0,    // proxEnable                                                       
+    0x0     // powerOn                                                          
+};  
+
+
+void initAPDS9130()
+{
+    // initialize all settings to default values.
+    apds9130_Params.proxIntegrationTime = 0xff; // PTIME default value.
+    apds9130_Params.waitTime = 0xff;    // 
+
+    apds9130_Params.proxPulseCount = 0x01;
+    apds9130_Params.proxDriveCurrent = 0x0; 
+    //apds9130_Params.proxDiodeSelect = 0x20;
+    apds9130_Params.proxGain = 0x0;
+
+
+    
+    // Set PDRIVE, PDIODE, and PGAIN values in CONTROL_ register.
+    uint8_t dataToSend = (apds9130_Params.proxDriveCurrent << PDRIVE_) |
+                         (apds9130_Params.proxGain << PGAIN_) | 
+                         (apds9130_Params.proxDiodeSelect << PDIODE_);
+    writeOne(I2C, apds9130_Params.deviceAddress_, CONTROL_, dataToSend); 
+}
+
+void initDataCollection()
+{
+    apds9130_Params.waitEnable = 1; 
+    apds9130_Params.proxEnable = 1;
+    apds9130_Params.powerOn    = 1;
+
+    // Set WEN, PEN, and PON values to initialize data collection.
+    uint8_t dataToSend = (apds9130_Params.waitEnable << WEN) |
+                         (apds9130_Params.proxEnable << PEN) |
+                         (apds9130_Params.powerOn    << PON);
+    writeOne(I2C, apds9130_Params.deviceAddress_, ENABLE_, dataToSend);
+}
+
+uint16_t readProxDataNonBlocking()
+{
+    uint8_t dataReceived[2];
+    read(I2C, apds9130_Params.deviceAddress_, PDATAL_, 2, dataReceived);
+    return (uint16_t)dataReceived[0] | ((uint16_t)dataReceived[1] << 8);
+}
+
+uint16_t readProxData()
+{
+    initDataCollection();
+    delayMS(12);
+    uint16_t val = readProxDataNonBlocking();
+    return val;
+}
+
---
+++ arduinoExample/apds-9130-driver.h
@@ -0,0 +1,95 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef APDS_9130_DRIVER_H
+#define APDS_9130_DRIVER_H 
+/**
+ * \project apds-9130-driver
+ * \brief   a generic driver providing the most basic functionality 
+ *          for the apds-9130 proximity sensor. In a nutshell, you 
+ *          implement the three functions read(...), write(...),
+ *          and delayMs(...) on the microcontroller of your choice, 
+ *          and the rest is done for you.
+ * \author Joshua Vasquez
+ * \date October 23, 2014
+*/
+
+#include <stdint.h>
+#include "embeddedCommon.h"  /// contains read(...) and write(...) fns.
+/// Important VL6180 settings
+struct sensorParams {
+    /*const*/ uint8_t deviceAddress_; // 7-bit address in struct to prevent
+                                         // name clashes for other sensors.
+    uint8_t proxIntegrationTime;
+    uint8_t waitTime;
+    uint8_t proxPulseCount;
+
+    uint8_t proxDriveCurrent;
+    /*const*/ uint8_t proxDiodeSelect;
+    uint8_t proxGain;
+    
+    /// Constants for performing a read.
+    uint8_t waitEnable;
+    uint8_t proxEnable;
+    uint8_t powerOn;
+}; 
+/*
+{
+    0x39,   // device address (cannot be changed)
+
+    0xff,   // proxIntegrationTime
+    0xff,   // waitTime
+    0x01,   // proxPulseCount
+
+    0x0,    // proxDriveCurrent
+    0x20,   // proxDiodeSelect (cannot be changed)
+    0x0,    // proxGain
+
+    0x0,    // waitEnable
+    0x0,    // proxEnable
+    0x0     // powerOn
+};
+*/
+
+/// Prototypes:
+void initAPDS9130();
+uint16_t readProxData();
+
+// The following two functions are used in tandem;
+void initDataCollection();  
+uint16_t readProxDataNonBlocking();
+
+
+/// Register Addresses: (datasheet page 16)
+const uint8_t ENABLE_   = 0x00;
+    /// ENABLE_ register bitfield offsets:
+    const uint8_t PON    = 0x00; 
+    const uint8_t PEN    = 0x02;
+    const uint8_t WEN    = 0x03;
+const uint8_t PTIME_    = 0x02;
+const uint8_t WTIME_    = 0x03;
+const uint8_t PILTL_    = 0x08;
+const uint8_t PILTH_    = 0x09;
+const uint8_t PIHTL_    = 0x0A;
+const uint8_t PIHTH_    = 0x0B;
+const uint8_t PERS_     = 0x0C;
+const uint8_t CONFIG_   = 0x0D;
+const uint8_t PPULSE_   = 0x0E;
+const uint8_t CONTROL_  = 0x0F;
+    /// CONTROL_ register bitfield offsets:
+    const uint8_t PGAIN_    = 0x02;
+    const uint8_t PDIODE_   = 0x04;
+    const uint8_t PDRIVE_   = 0x06;
+const uint8_t ID_       = 0x12;
+const uint8_t STATUS_   = 0x13;
+const uint8_t CH0DATA_  = 0x14;
+const uint8_t CH1DATA_  = 0x16;
+const uint8_t PDATAL_    = 0x18;
+const uint8_t PDATAH_   = 0x19;
+const uint8_t POFFSET_  = 0x1E;
+
+#endif // APDS_9130_H
+#ifdef __cplusplus
+}
+#endif
